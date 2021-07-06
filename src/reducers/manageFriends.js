056deee388f4e1ace65e59f0b90055173802dac7
ivnicolas <ikennanicolas@gmail.com>
2021-07-06 18:40:37 -0400

    commit tree 553acfed8d64fbca3b3186970f7a6f6e2a73d9ed

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends:[]}, action){
+    if(action.type=== "ADD_FRIEND" && action.friend){
+        return {friends:[...state.friends, action.friend] }
+    }if(action.type=== "REMOVE_FRIEND" && action.id){
+        return { friends: state.friends.filter(function( obj ) {
+            return obj.id !== action.id;
+          })} 
+    }else{
+        return state
+    }
 }
